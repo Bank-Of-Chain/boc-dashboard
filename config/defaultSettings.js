707da43ed36099d47f2157a8341bda9c9c003ede
李彬 <180505@nd.com>
2022-01-11 18:54:22 +0800

    init: first commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+const Settings = {
+  navTheme: 'realDark',
+  // 拂晓蓝
+  primaryColor: '#13C2C2',
+  layout: 'top',
+  contentWidth: 'Fixed',
+  fixedHeader: true,
+  fixSiderbar: true,
+  splitMenus: false,
+  colorWeak: false,
+  title: 'Bank Of Chain',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+export default Settings;
