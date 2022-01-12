d29e370a467124033b082f358c5f65a3299433f7
李彬 <180505@nd.com>
2022-01-12 11:38:14 +0800

    chore: add config for un root deployed

--- config/config.js
+++ config/config.js
@@ -5,6 +5,8 @@ import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 const { REACT_APP_ENV } = process.env;
 export default defineConfig({
+  base: '/dashboard/',
+  publicPath: '/dashboard/',
   hash: true,
   antd: {
     dark: true
