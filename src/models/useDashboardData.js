a99d3626c1f18cb504f9072b803a2fd61590e69f
李彬 <180505@nd.com>
2022-01-13 10:23:30 +0800

    feature: add api call and init data logic

--- src/models/useDashboardData.js
+++ src/models/useDashboardData.js
@@ -2,11 +2,28 @@ import {
   useRequest
 } from 'umi';
 import {
-  fetchData
+  fetchData,
+  getVaultDetails,
+  getVaultDailyData,
+  getVaultHourlyData,
+  getProtocols,
+  getStrategyById
 } from '@/services/dashboard-service';
 
+const dataMerge = () => {
+  return Promise.all([getVaultDetails()]).then((rs) => {
+    const [vaultDetail] = rs
+    const nextData = {
+      vaultDetail: vaultDetail.data
+    }
+    return {
+      data: nextData
+    }
+  })
+}
+
 export default function useDashboardData() {
-  const msg = useRequest(() => fetchData());
+  const msg = useRequest(() => dataMerge());
   return {
     dataSource: msg.data,
     reload: msg.run,
