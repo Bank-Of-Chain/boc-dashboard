93f7febd06f648b919fc3a8b88969cb79cf64846
李彬 <180505@nd.com>
2022-02-21 21:58:35 +0800

    chore: code updated

--- src/models/usePersonalData.js
+++ src/models/usePersonalData.js
@@ -102,7 +102,7 @@ export default function usePersonalData() {
     setLoading(true)
     // TODO: 发布时，需要删除
     // 先固定使用特定的地址
-    dataMerge('0x375d80da4271f5dcdf821802f981a765a0f11763').then(r => {
+    dataMerge('0x2346c6b1024e97c50370c783a66d80f577fe991d').then(r => {
       setData(r)
       setLoading(false)
     }).catch(() => setLoading(false))
