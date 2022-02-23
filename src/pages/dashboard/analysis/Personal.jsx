5f662d3202c0dedb8d943bd9717163f7fddd5663
李彬 <180505@nd.com>
2022-02-23 17:08:47 +0800

    feature: monthly profit add

--- src/pages/dashboard/analysis/Personal.jsx
+++ src/pages/dashboard/analysis/Personal.jsx
@@ -1,31 +1,29 @@
 import React, { Suspense, useEffect, useState } from 'react'
-import {useModel} from 'umi'
+import { useModel } from 'umi'
 import { getDecimals } from '@/apollo/client'
 
 // === Components === //
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { GridContent } from '@ant-design/pro-layout'
 import { Col, Row, Tooltip, Result, Card, Input } from 'antd'
-
-// === Components === //
 import { ChartCard } from './components/Charts'
 import { BarEchart, LineEchart } from '@/components/echarts'
 
 // === Utils === //
-import moment from 'moment';
-import filter from 'lodash/filter';
-import first from 'lodash/first';
-import last from 'lodash/last';
-import sumBy from 'lodash/sumBy';
-import _min from 'lodash/min';
-import _max from 'lodash/max';
-import isUndefined from 'lodash/isUndefined';
-import map from 'lodash/map';
-import groupBy from 'lodash/groupBy';
-import isEmpty from 'lodash/isEmpty';
-import compact from 'lodash/compact';
+import moment from 'moment'
+import filter from 'lodash/filter'
+import last from 'lodash/last'
+import sumBy from 'lodash/sumBy'
+import _min from 'lodash/min'
+import _max from 'lodash/max'
+import isUndefined from 'lodash/isUndefined'
+import map from 'lodash/map'
+import groupBy from 'lodash/groupBy'
+import isEmpty from 'lodash/isEmpty'
+import compact from 'lodash/compact'
+import reduce from 'lodash/reduce'
 import { toFixed } from '@/helper/number-format'
-import BN from 'bignumber.js';
+import BN from 'bignumber.js'
 import getLineEchartOpt from '@/components/echarts/options/line/getLineEchartOpt'
 
 const topColResponsiveProps = {
@@ -36,15 +34,14 @@ const topColResponsiveProps = {
   xl: 8,
 }
 
-
 const Personal = () => {
   const [totalAssets, setTotalAssets] = useState(0)
   const [bocBalance, setBOCBalance] = useState(0)
   const [profit, setProfit] = useState(0)
   const [totalProfit, setTotalProfit] = useState(0)
   const [depositedPercent, setDepositedPercent] = useState(0)
-  const {dataSource, loading} = useModel('usePersonalData')
-  const {initialState, setInitialState} = useModel('@@initialState')
+  const { dataSource, loading } = useModel('usePersonalData')
+  const { initialState, setInitialState } = useModel('@@initialState')
 
   const decimals = dataSource?.vaultSummary?.decimals
   const sharePrice = dataSource?.vaultSummary?.pricePerShare
@@ -58,23 +55,24 @@ const Personal = () => {
   const yearData = map(accountDailyDatasInYear, (i, index) => {
     return {
       ...i,
-      ...vaultDailyDatesInYear[index]
+      ...vaultDailyDatesInYear[index],
     }
   })
-  const totalAccumulatedProfit = sumBy(yearData, (o) => {
+  const totalAccumulatedProfit = sumBy(yearData, o => {
     if (o.hasOwnProperty('accumulatedProfit')) {
       return +o.accumulatedProfit
     }
     return 0
   })
   const totalTvl = sumBy(yearData, i => {
-    if(isUndefined(i.currentShares) || isUndefined(i.pricePerShare)) return 0
+    if (isUndefined(i.currentShares) || isUndefined(i.pricePerShare)) return 0
     return i.currentShares * i.pricePerShare
   })
-  const accountApyInYear = totalTvl === 0 ? 0 : (365 * 100 * totalAccumulatedProfit * 10 ** decimals / totalTvl)
+  const accountApyInYear =
+    totalTvl === 0 ? 0 : (365 * 100 * totalAccumulatedProfit * 10 ** decimals) / totalTvl
   useEffect(() => {
     if (!sharePrice || !shares || !decimals) return
-    setTotalAssets(sharePrice * shares / (10 ** decimals))
+    setTotalAssets((sharePrice * shares) / 10 ** decimals)
   }, [sharePrice, shares, decimals])
 
   useEffect(() => {
@@ -88,105 +86,182 @@ const Personal = () => {
   }, [totalAssets, depositedUSDT])
 
   useEffect(() => {
-    if ((!profit || profit === 0) || !accumulatedProfit) return
+    if (!profit || profit === 0 || !accumulatedProfit) return
     setTotalProfit(+profit + +accumulatedProfit)
   }, [profit, accumulatedProfit])
 
   useEffect(() => {
     if (!shares || !totalShares) return
-    setDepositedPercent(shares * 100 / totalShares)
+    setDepositedPercent((shares * 100) / totalShares)
   }, [shares, totalShares])
 
-  const monthOffset = moment().month() + 1;
-  const groupByMonth = groupBy(filter(yearData, i=> i.currentShares > 0 && i.pricePerShare > 0 && i.currentDepositedUSDT > 0), i=> moment(1000 * i.id).locale('en').format('MMM'))
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const monthOffset = moment().month() + 1
+  const groupByMonth = groupBy(
+    filter(yearData, i => i.currentShares > 0 && i.pricePerShare > 0 && i.currentDepositedUSDT > 0),
+    i =>
+      moment(1000 * i.id)
+        .locale('en')
+        .format('MMM'),
+  )
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
   const array = months.slice(monthOffset)
   const array1 = months.splice(0, monthOffset)
   const nextMonths = [...array, ...array1]
-  const ObtainedProfitArray = map(nextMonths, i => toFixed(`${sumBy(groupByMonth[i] || [], 'accumulatedProfit')}`, 10 ** decimals, 4))
-  const totalProfitArray = map(nextMonths, (i, index) => {
-    const monthArray =  groupByMonth[i]
-    if(isEmpty(monthArray)) return '0';
-    if(monthArray.length < 2) return '0';
-    const firstItem = first(monthArray)
+
+  if (yearData.length === 0) return <span />
+  const yearValidData = filter(yearData, i => BN(i.currentDepositedUSDT).gt(0))
+  const lastItem = last(yearValidData)
+
+  // 未实现的盈利
+  const value1 = BN(lastItem.pricePerShare)
+    .multipliedBy(lastItem.currentShares)
+    .div(10 ** decimals)
+    .minus(BN(lastItem.currentDepositedUSDT))
+  // 已实现的盈利
+  const value2 = reduce(
+    yearValidData,
+    (rs, item) => {
+      if (isEmpty(item.accumulatedProfit)) return rs
+      const nextValue = BN(item.accumulatedProfit)
+      return rs.plus(nextValue)
+    },
+    BN(0),
+  )
+  // 总盈利
+  const profitTotal = value1.plus(value2)
+
+  // 平均tvl
+  const avgTvl = reduce(
+    yearValidData,
+    (rs, item) => {
+      const nextPricePerShare = BN(item.pricePerShare)
+      const nextCurrentShares = BN(item.currentShares)
+      if (nextPricePerShare.lt(0) || nextCurrentShares.lt(0)) return rs
+      const nextValue = nextCurrentShares.multipliedBy(nextPricePerShare).div(10 ** decimals)
+      return rs.plus(nextValue)
+    },
+    BN(0),
+  ).div(yearValidData.length)
+
+  const value5 = map(nextMonths, i =>
+    toFixed(
+      `${reduce(
+        groupByMonth[i],
+        (rs, item) => {
+          if (isEmpty(item.accumulatedProfit)) return rs
+          const nextValue = BN(item.accumulatedProfit)
+          return rs.plus(nextValue)
+        },
+        BN(0),
+      )}`,
+      10 ** decimals,
+      4,
+    ),
+  )
+  const value51 = map(value5, (i, index) =>
+    sumBy(value5.slice(0, index + 1), o => parseFloat(o)).toFixed(4),
+  )
+  const profitOfLastDayOfMonth = map(nextMonths, i => {
+    const monthArray = groupByMonth[i]
+    if (isEmpty(monthArray)) return '0'
+    if (monthArray.length < 2) return '0'
     const lastItem = last(monthArray)
-    // 当前份额估值 减去 成本
-    const lastDayProfit = ((lastItem.currentShares * lastItem.pricePerShare / 10 ** decimals) - lastItem.currentDepositedUSDT) / 10 ** decimals
-    const firstDayProfit = ((firstItem.currentShares * firstItem.pricePerShare / 10 ** decimals) - firstItem.currentDepositedUSDT) / 10 ** decimals
-    return toFixed(`${lastDayProfit - firstDayProfit + 1 * ObtainedProfitArray[index]}`, 1, 4)
+    const lastDayProfit = BN(lastItem.currentShares)
+      .multipliedBy(BN(lastItem.pricePerShare))
+      .div(10 ** decimals)
+      .minus(lastItem.currentDepositedUSDT)
+      .div(10 ** decimals)
+    return toFixed(lastDayProfit, 1, 4)
   })
+  const monthProfitTotal = map(profitOfLastDayOfMonth, (i, index) =>
+    sumBy([i, value51[index]], o => parseFloat(o)).toFixed(4),
+  )
   const option = {
-    textStyle:{
-      color: '#fff'
+    textStyle: {
+      color: '#fff',
     },
-    color:['#91cc75', '#5470c6'],
-    legend: {
-      data: ['Total', 'Obtained'],
-      align: 'auto',
-      textStyle: {
-        color: '#fff'
-      }
-    },
+    color: ['#5470c6'],
     tooltip: {},
     xAxis: {
       data: nextMonths,
       axisLine: { onZero: true },
       splitLine: { show: false },
-      splitArea: { show: false }
+      splitArea: { show: false },
     },
     yAxis: {},
     grid: {
-      bottom: 100
+      bottom: 100,
     },
     series: [
-      {
-        name: 'Obtained',
-        type: 'bar',
-        stack: 'two',
-        data: ObtainedProfitArray
-      },
       {
         name: 'Total',
         type: 'bar',
         stack: 'one',
-        data:  totalProfitArray
+        data: map(monthProfitTotal, (i, index) => {
+          if (index === 0) return i
+          return (parseFloat(i) - parseFloat(monthProfitTotal[index - 1])).toFixed(4)
+        }),
       },
-    ]
+    ],
   }
 
-  const option1 = getLineEchartOpt(compact(map(yearData, i => {
-    if(isUndefined(i.currentShares) || isUndefined(i.pricePerShare)) return
-    return {
-      date: 1000 * i.dayTimestamp,
-      tvl: parseFloat(BN(i.currentShares).multipliedBy(BN(i.pricePerShare)).div(BN(10).pow(decimals * 2)).toFixed(4))
-    }
-  })), 'tvl', 'USDT', true, { format: 'MM-DD' })
+  const option1 = getLineEchartOpt(
+    compact(
+      map(yearData, i => {
+        if (isUndefined(i.currentShares) || isUndefined(i.pricePerShare)) return
+        return {
+          date: 1000 * i.dayTimestamp,
+          tvl: parseFloat(
+            BN(i.currentShares)
+              .multipliedBy(BN(i.pricePerShare))
+              .div(BN(10).pow(decimals * 2))
+              .toFixed(4),
+          ),
+        }
+      }),
+    ),
+    'tvl',
+    'USDT',
+    true,
+    {
+      format: 'MM-DD'
+    },
+  )
   if (isEmpty(initialState.address)) {
-    return (
-      <Result
-        status='500'
-        title='un connect'
-        subTitle='connect metamask firstly'
-      />
-    )
+    return <Result status='500' title='un connect' subTitle='connect metamask firstly' />
   }
-  console.log('option=', option)
   return (
     <GridContent>
       <Suspense fallback={null}>
-      <Row gutter={[24, 24]}>
-        <Col>
-          <Input placeholder="请输入用户地址" onChange={(e) => setInitialState({...initialState, address: e.target.value}) } />
-          <p>该输入框为测试使用，发布前需要删除</p>
-        </Col>
-      </Row>
+        <Row gutter={[24, 24]}>
+          <Col>
+            <Input
+              placeholder='请输入用户地址'
+              onChange={e => setInitialState({ ...initialState, address: e.target.value })}
+            />
+            <p>该输入框为测试使用，发布前需要删除</p>
+          </Col>
+        </Row>
         <Row gutter={[24, 24]}>
           <Col {...topColResponsiveProps}>
             <ChartCard
               bordered={false}
               title='Total Assets (USDT)'
               action={
-                <Tooltip title='Total Assets Amount'>
+                <Tooltip title='The value of shares currently'>
                   <InfoCircleOutlined />
                 </Tooltip>
               }
@@ -198,9 +273,9 @@ const Personal = () => {
           <Col {...topColResponsiveProps}>
             <ChartCard
               bordered={false}
-              title='Total Shares'
+              title='Shares'
               action={
-                <Tooltip title='Total Share Amount'>
+                <Tooltip title='Total shares amounts'>
                   <InfoCircleOutlined />
                 </Tooltip>
               }
@@ -215,11 +290,18 @@ const Personal = () => {
               loading={loading}
               title='APY'
               action={
-                <Tooltip title='last 1 year'>
+                <Tooltip title='Last 1 year'>
                   <InfoCircleOutlined />
                 </Tooltip>
               }
-              total={`${accountApyInYear.toFixed(2)}%`}
+              total={`${toFixed(
+                profitTotal
+                  .multipliedBy(365)
+                  .div(avgTvl)
+                  .div(yearValidData.length),
+                10 ** -2,
+                2,
+              )}%`}
               contentHeight={70}
             />
           </Col>
@@ -229,7 +311,7 @@ const Personal = () => {
               bordered={false}
               title='Profit (USDT)'
               action={
-                <Tooltip title='The Amount of Profit'>
+                <Tooltip title='The profits currently in the vault'>
                   <InfoCircleOutlined />
                 </Tooltip>
               }
@@ -244,7 +326,7 @@ const Personal = () => {
               bordered={false}
               title='Total Profit (USDT)'
               action={
-                <Tooltip title='The Amount of Total Profit'>
+                <Tooltip title='Profits taken out and profits currently in the vault'>
                   <InfoCircleOutlined />
                 </Tooltip>
               }
@@ -276,7 +358,7 @@ const Personal = () => {
           bordered={false}
           bodyStyle={{ paddingLeft: 0, paddingRight: 0, height: '600px' }}
           style={{ marginTop: 24 }}
-          title='Month Profit'
+          title='Monthly Profit'
         >
           <BarEchart option={option} style={{ height: '100%' }} />
         </Card>
@@ -285,7 +367,7 @@ const Personal = () => {
         <Card
           loading={loading}
           bordered={false}
-          bodyStyle={{ paddingLeft: 0, paddingRight: 0, height: '600px' }}
+          bodyStyle={{ height: '600px' }}
           style={{ marginTop: 24 }}
           title='Daily TVL'
         >
