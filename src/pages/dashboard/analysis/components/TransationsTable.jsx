874727c37c8d7e30b47957991b239bc44026c227
李彬 <180505@nd.com>
2022-03-17 10:21:41 +0800

    feature: page adapter

--- src/pages/dashboard/analysis/components/TransationsTable.jsx
+++ src/pages/dashboard/analysis/components/TransationsTable.jsx
@@ -1,7 +1,10 @@
-import { Card, Table, Tooltip,Button } from 'antd'
+import { Card, Table, Tooltip, Button } from 'antd'
 import React from 'react'
 import { useModel } from 'umi'
-import { MoreOutlined } from '@ant-design/icons';
+import { MoreOutlined } from '@ant-design/icons'
+
+// === Components === //
+import { Desktop, Tablet, Mobile } from '@/components/Container/Container'
 
 // === Utils === //
 import moment from 'moment'
@@ -15,12 +18,11 @@ const TransationsTable = ({ loading, visitData, dropdownGroup }) => {
       title: 'Date',
       dataIndex: 'timestamp',
       key: 'timestamp',
-      width: 200,
       render: text => (
-        <Tooltip
-          title={moment(1000 * text).format('yyyy-MM-DD HH:mm:ss')}
-        >
-          {moment(1000 * text).locale('en').fromNow()}
+        <Tooltip title={moment(1000 * text).format('yyyy-MM-DD HH:mm:ss')}>
+          {moment(1000 * text)
+            .locale('en')
+            .fromNow()}
         </Tooltip>
       ),
     },
@@ -28,7 +30,7 @@ const TransationsTable = ({ loading, visitData, dropdownGroup }) => {
       title: 'Account',
       dataIndex: 'from',
       key: 'from',
-      width: 400,
+      width: '22rem',
       ellipsis: {
         showTitle: false,
       },
@@ -47,63 +49,123 @@ const TransationsTable = ({ loading, visitData, dropdownGroup }) => {
       title: 'Method',
       dataIndex: 'method',
       key: 'method',
-      width: 100,
-      render: text => <span style={{ color: text === 'Deposit' ? '#80FF80' : '#FF8080' }}>{text}</span>,
+      render: text => (
+        <span style={{ color: text === 'Deposit' ? '#80FF80' : '#FF8080' }}>{text}</span>
+      ),
     },
     {
       title: 'Shares',
-      width: 100,
-      render: (text, item) =>
-        `${toFixed(item.shares, getDecimals(), 2)}`,
+      render: (text, item) => `${toFixed(item.shares, getDecimals(), 2)}`,
     },
     {
       title: 'Shares Value (USDT)',
-      width: 200,
-      render: (text, item) =>
-        `${toFixed(item.shareValue, getDecimals(), 2)}`,
+      width: '10rem',
+      render: (text, item) => `${toFixed(item.shareValue, getDecimals(), 2)}`,
     },
     {
       title: 'Detail',
       dataIndex: 'id',
       key: 'id',
-      width: 100,
+      width: '4rem',
       ellipsis: {
         showTitle: false,
       },
       render: text => (
-        <Button type="dashed" icon={<MoreOutlined />} size={'Default'}
-                target={'_blank'}
-                href={`${CHAIN_BROWSER_URL[initialState.chain]}/tx/${text}`}
-                rel='noreferrer'
+        <Button
+          type='dashed'
+          icon={<MoreOutlined />}
+          size={'Default'}
+          target={'_blank'}
+          href={`${CHAIN_BROWSER_URL[initialState.chain]}/tx/${text}`}
+          rel='noreferrer'
         />
       ),
     },
   ]
   return (
-    <Card
-      loading={loading}
-      bordered={false}
-      title='Transactions'
-      extra={dropdownGroup}
-      style={{
-        height: '100%',
-        marginTop: 32,
-      }}
-    >
-      <Table
-        rowKey={record => record.id}
-        size='small'
-        scroll={{ x: 1300 }}
-        columns={columns}
-        dataSource={visitData}
-        pagination={{
-          style: {
-            marginBottom: 0,
-          },
-          pageSize: 10,
-        }}
-      />
-    </Card>
+    <div>
+      <Desktop>
+        <Card
+          loading={loading}
+          bordered={false}
+          title='Transactions'
+          extra={dropdownGroup}
+          style={{
+            height: '100%',
+            marginTop: 32,
+          }}
+        >
+          <Table
+            rowKey={record => record.id}
+            columns={columns}
+            dataSource={visitData}
+            pagination={{
+              style: {
+                marginBottom: 0,
+              },
+              pageSize: 10,
+            }}
+          />
+        </Card>
+      </Desktop>
+      <Tablet>
+        <Card
+          loading={loading}
+          bordered={false}
+          size='small'
+          title='Transactions'
+          extra={dropdownGroup}
+          style={{
+            height: '100%',
+            marginTop: 32,
+          }}
+        >
+          <Table
+            rowKey={record => record.id}
+            size='small'
+            rowClassName='tablet-font-size'
+            scroll={{ x: 900 }}
+            columns={columns}
+            dataSource={visitData}
+            pagination={{
+              style: {
+                marginBottom: 0,
+              },
+              pageSize: 10,
+            }}
+          />
+        </Card>
+      </Tablet>
+      <Mobile>
+        <Card
+          loading={loading}
+          bordered={false}
+          title='Transactions'
+          size='small'
+          extra={dropdownGroup}
+          style={{
+            height: '100%',
+            marginTop: 32,
+          }}
+        >
+          <Table
+            compact
+            rowKey={record => record.id}
+            size='small'
+            rowClassName='mobile-font-size'
+            scroll={{ x: 900 }}
+            columns={columns}
+            dataSource={visitData}
+            pagination={{
+              style: {
+                marginBottom: 0,
+              },
+              pageSize: 10,
+            }}
+          />
+        </Card>
+      </Mobile>
+    </div>
   )
 }
 
