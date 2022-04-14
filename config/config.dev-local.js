3e4b2b0837ee344af2221c4f337ec18c6321b10a
李彬 <180505@nd.com>
2022-04-14 12:01:35 +0800

    chore: add usdi_address in config

--- config/config.dev-local.js
+++ config/config.dev-local.js
@@ -22,11 +22,6 @@ export default defineConfig({
       1: 'https://api.thegraph.com/subgraphs/name/naruduo/my-subgraph-eth',
       56: 'https://api.thegraph.com/subgraphs/name/naruduo/my-subgraph-bsc',
       137: 'https://api.thegraph.com/subgraphs/name/naruduo/my-subgraph'
-    },
-    VAULT_ADDRESS: {
-      1: '0x70bDA08DBe07363968e9EE53d899dFE48560605B',
-      56: '0x937f8bb67B61ad405D56BD3e1094b172D96B4038',
-      137: '0xe47F0396CfCB8134A791246924171950f1a83053'
     }
   }
 });
