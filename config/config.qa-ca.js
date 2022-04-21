e7f9b5014d4294cd4335c2fb1b1227014c241881
吴政衍 <160201@nd.com>
2022-04-21 23:26:34 +0800

    chore: update qa-ca config

--- config/config.qa-ca.js
+++ config/config.qa-ca.js
@@ -3,13 +3,13 @@ import {
   defineConfig
 } from 'umi';
 export default defineConfig({
-  base: '/dashboard/',
-  publicPath: '/dashboard/',
+  base: '/',
+  publicPath: '/',
   define: {
     ENV_INDEX: 'debug',
-    API_SERVER: 'http://192.168.60.12:8080',
-    DASHBOARD_ROOT: 'http://192.168.60.12/dashboard',
-    IMAGE_ROOT:'http://192.168.60.12',
+    API_SERVER: 'https://service-inte.bankofchain.io/server',
+    DASHBOARD_ROOT: 'https://dashboard-inte.bankofchain.io',
+    IMAGE_ROOT:'https://inte.bankofchain.io',
     SUB_GRAPH_URL: {
       1: 'https://api.thegraph.com/subgraphs/name/naruduo/my-subgraph-eth',
       56: 'https://api.thegraph.com/subgraphs/name/naruduo/my-subgraph-bsc',
@@ -18,7 +18,12 @@ export default defineConfig({
     VAULT_ADDRESS: {
       1: '0x66F625B8c4c635af8b74ECe2d7eD0D58b4af3C3d',
       56: '0x937f8bb67B61ad405D56BD3e1094b172D96B4038',
-      137: '0xe47F0396CfCB8134A791246924171950f1a83053'
+      137: '0x204d2e5c581506e939295daf99079b590ace906e'
+    },
+    USDI_ADDRESS: {
+      1: '',
+      56: '',
+      137: '0x6dc1bebb8e0881aca6f082f5f53dd740c2ddf379'
     }
   },
 });
