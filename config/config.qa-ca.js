bf2eba993ebe0a1cacc6e7eb8fdf3e9ebfadbcb5
吴政衍 <160201@nd.com>
2022-04-25 21:24:26 +0800

    chore: update config

--- config/config.qa-ca.js
+++ config/config.qa-ca.js
@@ -18,12 +18,12 @@ export default defineConfig({
     VAULT_ADDRESS: {
       1: '0x66F625B8c4c635af8b74ECe2d7eD0D58b4af3C3d',
       56: '0x937f8bb67B61ad405D56BD3e1094b172D96B4038',
-      137: '0x204d2e5c581506e939295daf99079b590ace906e'
+      137: '0x7a6e91c3c4fcb9157a27eb901210aa1df9d05b45'
     },
     USDI_ADDRESS: {
       1: '',
       56: '',
-      137: '0x6dc1bebb8e0881aca6f082f5f53dd740c2ddf379'
+      137: '0xb334795bf50e4943d076dfb38d8c1a50f9f5a101'
     }
   },
 });
