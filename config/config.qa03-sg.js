3ea729c995936fd781319eb21659c743c5cb9013
吴政衍 <160201@nd.com>
2022-05-07 18:57:00 +0800

    feat: update header menu and config

--- config/config.qa03-sg.js
+++ config/config.qa03-sg.js
@@ -7,23 +7,36 @@ export default defineConfig({
   publicPath: '/',
   define: {
     ENV_INDEX: 'debug',
-    API_SERVER: 'https://service-qa02-sg.bankofchain.io',
-    DASHBOARD_ROOT: 'https://dashboard-qa02-sg.bankofchain.io',
-    IMAGE_ROOT: 'https://qa02-sg.bankofchain.io',
-    SUB_GRAPH_URL: {
-      1: 'https://qa02-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
-      56: 'http://192.168.75.33:8000/subgraphs/name/boc-v1_5/subgraph',
-      137: 'http://192.168.67.39:8000/subgraphs/name/boc-v1_5/subgraph',
+    API_SERVER: 'https://service-qa03-sg.bankofchain.io',
+    DASHBOARD_ROOT: 'https://dashboard-qa03-sg.bankofchain.io',
+    IMAGE_ROOT: 'https://qa03-sg.bankofchain.io',
+    USDI: {
+      SUB_GRAPH_URL: {
+        1: 'https://qa03-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
+        56: 'http://192.168.75.33:8000/subgraphs/name/boc-v1_5/subgraph',
+        137: 'http://192.168.67.39:8000/subgraphs/name/boc-v1_5/subgraph',
+      },
+      VAULT_ADDRESS: {
+        1: '0x9BcC604D4381C5b0Ad12Ff3Bf32bEdE063416BC7',
+        56: '0xFEE2d383Ee292283eC43bdf0fa360296BE1e1149',
+        137: '0x204d2e5c581506e939295daf99079b590ace906e'
+      },
+      USDI_ADDRESS: {
+        1: '0xf090f16dEc8b6D24082Edd25B1C8D26f2bC86128',
+        56: '0xE3e7A4B35574Ce4b9Bc661cD93e8804Da548932a',
+        137: '0x6dc1bebb8e0881aca6f082f5f53dd740c2ddf379'
+      }
     },
-    VAULT_ADDRESS: {
-      1: '0x9BcC604D4381C5b0Ad12Ff3Bf32bEdE063416BC7',
-      56: '0xFEE2d383Ee292283eC43bdf0fa360296BE1e1149',
-      137: '0x204d2e5c581506e939295daf99079b590ace906e'
-    },
-    USDI_ADDRESS: {
-      1: '0xf090f16dEc8b6D24082Edd25B1C8D26f2bC86128',
-      56: '0xE3e7A4B35574Ce4b9Bc661cD93e8804Da548932a',
-      137: '0x6dc1bebb8e0881aca6f082f5f53dd740c2ddf379'
+    ETHI: {
+      SUB_GRAPH_URL: {
+        1: 'https://qa02-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
+      },
+      VAULT_ADDRESS: {
+        1: '0x9BcC604D4381C5b0Ad12Ff3Bf32bEdE063416BC7',
+      },
+      ETHI_ADDRESS: {
+        1: '0xf090f16dEc8b6D24082Edd25B1C8D26f2bC86128',
+      }
     }
   },
 });
