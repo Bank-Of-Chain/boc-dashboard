1fee658c9afeb5252fe32e25e4209f98d7ced31e
李彬 <180505@nd.com>
2022-05-20 10:59:53 +0800

    chore: config update

--- config/config.stage-sg.js
+++ config/config.stage-sg.js
@@ -8,20 +8,33 @@ export default defineConfig({
     API_SERVER: 'https://service-stage-sg.bankofchain.io',
     DASHBOARD_ROOT: 'https://dashboard-stage-sg.bankofchain.io',
     IMAGE_ROOT:'https://stage-sg.bankofchain.io',
-    SUB_GRAPH_URL: {
-      1: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
-      56: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
-      137: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph'
+    USDI: {
+      SUB_GRAPH_URL: {
+        1: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
+        56: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph',
+        137: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph'
+      },
+      VAULT_ADDRESS: {
+        1: '0xd5C7A01E49ab534e31ABcf63bA5a394fF1E5EfAC',
+        56: '0x76609c83dD684F0D4c0F0c9849db0a1b5a96CAB2',
+        137: '0x85f93384BAd10d7751Fcc3bBD8F8710db3190700'
+      },
+      USDI_ADDRESS: {
+        1: '0xBe15Eed7D8e91D20263d4521c9eB0F4e3510bfBF',
+        56: '0xCFC6E8577a414f561D459fC4a030e3463A500d29',
+        137: '0x1C9f974DF781C6EB3764F21Fe961ba38305213df'
+      },
     },
-    VAULT_ADDRESS: {
-      1: '0x9bcc604d4381c5b0ad12ff3bf32bede063416bc7',
-      56: '0xFEE2d383Ee292283eC43bdf0fa360296BE1e1149',
-      137: '0x204d2e5c581506e939295daf99079b590ace906e'
-    },
-    USDI_ADDRESS: {
-      1: '0xf090f16dec8b6d24082edd25b1c8d26f2bc86128',
-      56: '0xE3e7A4B35574Ce4b9Bc661cD93e8804Da548932a',
-      137: '0x6dc1bebb8e0881aca6f082f5f53dd740c2ddf379'
+    ETHI: {
+      SUB_GRAPH_URL: {
+        1: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-ethi',
+      },
+      VAULT_ADDRESS: {
+        1: '0xDae16f755941cbC0C9D240233a6F581d1734DaA2',
+      },
+      ETHI_ADDRESS: {
+        1: '0x8cB9Aca95D1EdebBfe6BD9Da4DC4a2024457bD32',
+      }
     }
   },
 });
