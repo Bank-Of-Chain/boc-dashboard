19b5cbb760475c1e64316efa868892e529887bec
韩旭 <js_bin@163.com>
2022-07-01 17:56:53 +0800

    chore: config update

--- config/config.stage-sg.js
+++ config/config.stage-sg.js
@@ -16,17 +16,17 @@ export default defineConfig({
     },
     USDI: {
       SUB_GRAPH_URL: {
-        1: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-eth',
+        1: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph-eth',
         56: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-bnb',
         137: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-polygon'
       },
       VAULT_ADDRESS: {
-        1: '0x359570B3a0437805D0a71457D61AD26a28cAC9A2',
+        1: '0xd5C7A01E49ab534e31ABcf63bA5a394fF1E5EfAC',
         56: '0x76609c83dD684F0D4c0F0c9849db0a1b5a96CAB2',
         137: '0xd3feAe6c4fdfDE73Bd2fE99c8fE6944904DAA68A'
       },
       USDI_ADDRESS: {
-        1: '0x67aD6EA566BA6B0fC52e97Bc25CE46120fdAc04c',
+        1: '0xC7143d5bA86553C06f5730c8dC9f8187a621A8D4',
         56: '0xCFC6E8577a414f561D459fC4a030e3463A500d29',
         137: '0x8DEb399a86f28f62f0F24daF56c4aDD8e57EEcD5'
       },
@@ -36,10 +36,10 @@ export default defineConfig({
         1: 'https://stage-sg-subgraph.bankofchain.io/subgraphs/name/boc-v1_5/subgraph-ethi',
       },
       VAULT_ADDRESS: {
-        1: '0xb868Cc77A95a65F42611724AF05Aa2d3B6Ec05F2',
+        1: '0xDae16f755941cbC0C9D240233a6F581d1734DaA2',
       },
       ETHI_ADDRESS: {
-        1: '0x7Cf4be31f546c04787886358b9486ca3d62B9acf',
+        1: '0x162700d1613DfEC978032A909DE02643bC55df1A',
       }
     }
   },
