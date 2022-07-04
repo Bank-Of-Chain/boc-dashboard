96811bd40319e3fae217f0ffa086e79cc5985908
韩旭 <js_bin@163.com>
2022-07-04 19:56:39 +0800

    chore: config address update

--- config/config.dev-local.js
+++ config/config.dev-local.js
@@ -1,5 +1,17 @@
 // https://umijs.org/config/
-import { defineConfig } from 'umi';
+import { defineConfig } from 'umi'
+
+const ETHI_FOR_ETH = '0x33E45b187da34826aBCEDA1039231Be46f1b05Af'
+
+const USDI_FOR_ETH = '0x67aD6EA566BA6B0fC52e97Bc25CE46120fdAc04c'
+const USDI_FOR_BSC = ''
+const USDI_FOR_MATIC = ''
+
+const ETHI_VAULT = '0x70E5370b8981Abc6e14C91F4AcE823954EFC8eA3'
+const USDI_VAULT_FOR_ETH = '0x359570B3a0437805D0a71457D61AD26a28cAC9A2'
+const USDI_VAULT_FOR_BSC = ''
+const USDI_VAULT_FOR_MATIC = ''
+
 export default defineConfig({
   base: '/',
   publicPath: '/',
@@ -13,31 +25,31 @@ export default defineConfig({
     babelPlugins: [],
     babelOptions: {},
   },
-  define:{
+  define: {
     ENV_INDEX: 'dev-local',
     API_SERVER: 'http://192.168.75.31:8080',
     DASHBOARD_ROOT: 'http://localhost:8000',
-    IMAGE_ROOT:'https://web-v1.bankofchain.io',
+    IMAGE_ROOT: 'https://web-v1.bankofchain.io',
     RPC_URL: {
-      1: "http://localhost:8545",
-      56: "https://bsc-dataseed.binance.org",
-      137: "https://rpc-mainnet.maticvigil.com"
+      1: 'http://localhost:8545',
+      56: 'https://bsc-dataseed.binance.org',
+      137: 'https://rpc-mainnet.maticvigil.com',
     },
     USDI: {
       SUB_GRAPH_URL: {
         1: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-bnb',
         56: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-bnb',
-        137: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-polygon'
+        137: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-polygon',
       },
       VAULT_ADDRESS: {
-        1: '0x94fFA1C7330845646CE9128450F8e6c3B5e44F86',
-        56: '0x76609c83dD684F0D4c0F0c9849db0a1b5a96CAB2',
-        137: '0xd3feAe6c4fdfDE73Bd2fE99c8fE6944904DAA68A'
+        1: USDI_VAULT_FOR_ETH,
+        56: USDI_VAULT_FOR_BSC,
+        137: USDI_VAULT_FOR_MATIC,
       },
       USDI_ADDRESS: {
-        1: '0xB22C255250d74B0ADD1bfB936676D2a299BF48Bd',
-        56: '0xCFC6E8577a414f561D459fC4a030e3463A500d29',
-        137: '0x8DEb399a86f28f62f0F24daF56c4aDD8e57EEcD5'
+        1: USDI_FOR_ETH,
+        56: USDI_FOR_BSC,
+        137: USDI_FOR_MATIC,
       },
     },
     ETHI: {
@@ -45,11 +57,11 @@ export default defineConfig({
         1: 'http://192.168.75.33:8000/subgraphs/name/boc-v1_5/subgraph-ethi',
       },
       VAULT_ADDRESS: {
-        1: '0xDae16f755941cbC0C9D240233a6F581d1734DaA2',
+        1: ETHI_VAULT,
       },
       ETHI_ADDRESS: {
-        1: '0x8cB9Aca95D1EdebBfe6BD9Da4DC4a2024457bD32',
-      }
-    }
-  }
-});
+        1: ETHI_FOR_ETH,
+      },
+    },
+  },
+})
--- config/config.pr-sg.js
+++ config/config.pr-sg.js
@@ -1,33 +1,45 @@
 // https://umijs.org/config/
-import { defineConfig } from 'umi';
+import { defineConfig } from 'umi'
+
+const ETHI_FOR_ETH = '0x8cB9Aca95D1EdebBfe6BD9Da4DC4a2024457bD32'
+
+const USDI_FOR_ETH = '0xBe15Eed7D8e91D20263d4521c9eB0F4e3510bfBF'
+const USDI_FOR_BSC = '0xCFC6E8577a414f561D459fC4a030e3463A500d29'
+const USDI_FOR_MATIC = '0x8DEb399a86f28f62f0F24daF56c4aDD8e57EEcD5'
+
+const ETHI_VAULT = '0xDae16f755941cbC0C9D240233a6F581d1734DaA2'
+const USDI_VAULT_FOR_ETH = '0xd5C7A01E49ab534e31ABcf63bA5a394fF1E5EfAC'
+const USDI_VAULT_FOR_BSC = '0x76609c83dD684F0D4c0F0c9849db0a1b5a96CAB2'
+const USDI_VAULT_FOR_MATIC = '0xd3feAe6c4fdfDE73Bd2fE99c8fE6944904DAA68A'
+
 export default defineConfig({
   base: '/',
   publicPath: '/',
-  define:{
+  define: {
     ENV_INDEX: 'pr-sg',
     API_SERVER: 'https://service-v1.bankofchain.io',
     DASHBOARD_ROOT: 'https://dashboard-v1.bankofchain.io',
-    IMAGE_ROOT:'https://web-v1.bankofchain.io',
+    IMAGE_ROOT: 'https://web-v1.bankofchain.io',
     RPC_URL: {
-      1: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      56: "https://bsc-dataseed.binance.org",
-      137: "https://polygon-rpc.com"
+      1: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      56: 'https://bsc-dataseed.binance.org',
+      137: 'https://polygon-rpc.com',
     },
     USDI: {
       SUB_GRAPH_URL: {
         1: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-ethereum',
         56: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-bnb',
-        137: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-polygon'
+        137: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-polygon',
       },
       VAULT_ADDRESS: {
-        1: '0xd5C7A01E49ab534e31ABcf63bA5a394fF1E5EfAC',
-        56: '0x76609c83dD684F0D4c0F0c9849db0a1b5a96CAB2',
-        137: '0xd3feAe6c4fdfDE73Bd2fE99c8fE6944904DAA68A'
+        1: USDI_VAULT_FOR_ETH,
+        56: USDI_VAULT_FOR_BSC,
+        137: USDI_VAULT_FOR_MATIC,
       },
       USDI_ADDRESS: {
-        1: '0xBe15Eed7D8e91D20263d4521c9eB0F4e3510bfBF',
-        56: '0xCFC6E8577a414f561D459fC4a030e3463A500d29',
-        137: '0x8DEb399a86f28f62f0F24daF56c4aDD8e57EEcD5'
+        1: USDI_FOR_ETH,
+        56: USDI_FOR_BSC,
+        137: USDI_FOR_MATIC,
       },
     },
     ETHI: {
@@ -35,11 +47,11 @@ export default defineConfig({
         1: 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-ethi',
       },
       VAULT_ADDRESS: {
-        1: '0xDae16f755941cbC0C9D240233a6F581d1734DaA2',
+        1: ETHI_VAULT,
       },
       ETHI_ADDRESS: {
-        1: '0x8cB9Aca95D1EdebBfe6BD9Da4DC4a2024457bD32',
-      }
-    }
+        1: ETHI_FOR_ETH,
+      },
+    },
   },
-});
+})
