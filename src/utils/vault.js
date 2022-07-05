6358d41e58cf1848b5107ee97e731c1d2aff03ae
韩旭 <js_bin@163.com>
2022-07-05 22:38:49 +0800

    chore: add vault_buffer_address

--- src/utils/vault.js
+++ src/utils/vault.js
@@ -4,11 +4,13 @@ export const getVaultConfig = (chain, vault) => {
   return {
     [VAULT_TYPE.USDi]: {
       vaultAddress: USDI.VAULT_ADDRESS[chain],
-      tokenAddress: USDI.USDI_ADDRESS[chain]
+      tokenAddress: USDI.USDI_ADDRESS[chain],
+      vaultBufferAddress: USDI.VAULT_BUFFER_ADDRESS[chain]
     },
     [VAULT_TYPE.ETHi]: {
       vaultAddress: ETHI.VAULT_ADDRESS[chain],
-      tokenAddress: ETHI.ETHI_ADDRESS[chain]
+      tokenAddress: ETHI.ETHI_ADDRESS[chain],
+      vaultBufferAddress: ETHI.VAULT_BUFFER_ADDRESS[chain]
     }
   }[vault]
 }
