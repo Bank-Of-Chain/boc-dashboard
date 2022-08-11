2e8060392044f8594ea37520342ebd6c896102c9
jiangcx <786255@nd.com>
2022-08-11 18:49:00 +0800

    feat: eslint-config-prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,2 +1,9 @@
-{
+module.exports = {
+  printWidth: 100, // 超过最大值换行
+  semi: false, // 句尾添加分号
+  singleQuote: true, // 使用单引号代替双引号
+  endOfLine: 'crlf', // 结尾是 \n \r \n\r auto
+  jsxSingleQuote: false, // 在jsx中使用单引号代替双引号
+  parser: 'babel', // 格式化的解析器，默认是babylon
+  trailingComma: 'none' // 在对象或数组最后一个元素后面是否加逗号
 }
