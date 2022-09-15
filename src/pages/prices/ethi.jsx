685ec6d54ee9d0e61a07c3ad0dbb07ca916ed54a
韩旭gogogo <js_bin@163.com>
2022-09-15 15:58:38 +0800

    feature: echart component add

---
+++ src/pages/prices/ethi.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+// === Utils === //
+import { useModel, useRequest } from 'umi'
+import getLineEchartOpt from '@/components/echarts/options/line/getLineEchartOpt'
+import map from 'lodash/map'
+
+// === Components === //
+import { Row, Col } from 'antd'
+import { LineEchart } from '@/components/echarts'
+
+// === Services === //
+import { getPrices } from '@/services/price-service'
+import BigNumber from 'bignumber.js'
+
+const ETHIPrice = () => {
+  const { initialState } = useModel('@@initialState')
+  const { data } = useRequest(() => getPrices(initialState.chain, initialState.vaultAddress), {
+    manual: false,
+    paginated: true,
+    formatResult: resp => {
+      const { content } = resp
+      return {
+        total: resp.totalElements,
+        list: map(content, i => {
+          return i
+        })
+      }
+    }
+  })
+  const showData = map(data?.list, i => {
+    return {
+      value: i.rate,
+      date: i.validateTime
+    }
+  })
+  const tvlEchartOpt = getLineEchartOpt(showData, 'value', 'date', {
+    format: 'MM-DD',
+    yAxisMin: value => value.min,
+    yAxisMax: value => value.max,
+    xAxis: {
+      axisTick: {
+        alignWithLabel: true
+      }
+    },
+    yAxis: {
+      axisLabel: {
+        formatter: v => {
+          const value = new BigNumber(v).minus(1e17)
+          return `${value.gt(0) ? '+' : ''} ${value.toFormat()}`
+        }
+      }
+    },
+    dataZoom: [
+      {
+        end: 100,
+        start: showData.length < 30 ? 0 : 100 * (1 - 30 / showData.length)
+      }
+    ]
+  })
+  return (
+    <Row>
+      <Col span={24}>
+        <LineEchart option={tvlEchartOpt} style={{ minHeight: '37rem', width: '100%' }} />
+      </Col>
+    </Row>
+  )
+}
+export default ETHIPrice
