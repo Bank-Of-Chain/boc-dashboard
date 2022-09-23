e5ae71914e8503fb0139557f2d3603228e37e397
韩旭gogogo <js_bin@163.com>
2022-09-23 14:14:50 +0800

    feature: personal profits

---
+++ src/services/profits-service.js
@@ -0,0 +1,42 @@
+// === Utils === //
+import { request } from 'umi'
+import isEmpty from 'lodash/isEmpty'
+
+/**
+ * get segment profits in DAY/WEEK/MONTH
+ * @param {*} address user address
+ * @param {*} chainId
+ * @param {*} tokenType USDI or ETHI
+ * @param {*} segmentType DAY OR WEEK OR MONTH
+ * @returns
+ */
+export const getSegmentProfit = (address, chainId, tokenType, segmentType) => {
+  if (isEmpty(address) || isEmpty(chainId) || isEmpty(tokenType) || isEmpty(segmentType))
+    return Promise.reject('address & chainId & tokenType & segmentType must not be null')
+
+  return request(`${API_SERVER}/account_profit_history/segment_profit/account/${address}`, {
+    params: {
+      chainId,
+      tokenType,
+      segmentType
+    }
+  })
+}
+
+/**
+ * get latest profits
+ * @param {*} address user address
+ * @param {*} chainId
+ * @param {*} tokenType USDI or ETHI
+ * @returns
+ */
+export const getLatestProfit = (address, chainId, tokenType) => {
+  if (isEmpty(address) || isEmpty(chainId) || isEmpty(tokenType)) return Promise.reject('address & chainId & tokenType must not be null')
+
+  return request(`${API_SERVER}/account_profit_history/last_time_profit/account/${address}`, {
+    params: {
+      chainId,
+      tokenType
+    }
+  })
+}
