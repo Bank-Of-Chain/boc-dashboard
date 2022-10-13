504ceb9d8686babbbd11e7d381bd7db0fa320c86
韩旭gogogo <js_bin@163.com>
2022-10-13 15:31:44 +0800

    test: testcases add

--- src/__tests__/constants/strategies.test.js
+++ src/__tests__/constants/strategies.test.js
@@ -1,13 +1,13 @@
 import { USDI_ETH_STRATEGIES_MAP, USDI_MATIC_STRATEGIES_MAP, ETHI_ETH_STRATEGIES_MAP } from '@/constants/strategies'
 
-test('eth usdi has 16 platform', () => {
-  expect(USDI_ETH_STRATEGIES_MAP.length).toBe(16)
+test('eth usdi has 17 platform', () => {
+  expect(USDI_ETH_STRATEGIES_MAP.length).toBe(17)
 })
 
 test('matic usdi has 12 platform', () => {
   expect(USDI_MATIC_STRATEGIES_MAP.length).toBe(12)
 })
 
-test('eth ethi has 7 platform', () => {
-  expect(ETHI_ETH_STRATEGIES_MAP.length).toBe(7)
+test('eth ethi has 8 platform', () => {
+  expect(ETHI_ETH_STRATEGIES_MAP.length).toBe(8)
 })
