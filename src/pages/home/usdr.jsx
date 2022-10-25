cd133d0f9a9e05a94fe5db7a38746984c93859ad
tianxiuali <627786255@qq.com>
2022-10-25 11:25:46 +0800

    feat: update field

--- src/pages/home/usdr.jsx
+++ src/pages/home/usdr.jsx
@@ -321,7 +321,7 @@ const UsdrHome = props => {
     color: ['#A68EFE', '#5470c6', '#91cc75'],
     series: [
       {
-        data: map(sampleApy.result?.data, item => (item.lossApy * 100).toFixed(2)),
+        data: map(sampleApy.result?.data, item => (item.apy * 100).toFixed(2)),
         type: 'line',
         lineStyle: {
           width: 5,
