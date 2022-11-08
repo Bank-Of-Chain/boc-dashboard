cc945ac37caa6e8ae5433d4ae43c7776fe6d551d
韩旭gogogo <js_bin@163.com>
2022-11-08 16:47:42 +0800

    refactor: code update

--- src/constants/vault.js
+++ src/constants/vault.js
@@ -2,7 +2,9 @@ export const VAULT_TYPE = {
   USDi: 'usdi',
   ETHi: 'ethi',
   USDr: 'usdr',
-  ETHr: 'ethr'
+  ETHr: 'ethr',
+  USDrV2: 'usdrV2',
+  ETHrV2: 'ethrV2'
 }
 
 export const TOKEN_DISPLAY_DECIMALS = 2
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,6 +6,8 @@ import ETHi from './ethi'
 import USDi from './usdi'
 import ETHr from './ethr'
 import USDr from './usdr'
+import USDrV2 from './usdrV2'
+import ETHrV2 from './ethrV2'
 import NoFoundPage from './../404'
 
 // === Constants === //
@@ -20,7 +22,9 @@ export default function Home(props) {
     [VAULT_TYPE.USDi]: USDi,
     [VAULT_TYPE.ETHi]: ETHi,
     [VAULT_TYPE.ETHr]: ETHr,
-    [VAULT_TYPE.USDr]: USDr
+    [VAULT_TYPE.USDr]: USDr,
+    [VAULT_TYPE.ETHrV2]: ETHrV2,
+    [VAULT_TYPE.USDrV2]: USDrV2
   }[initialState.vault]
 
   if (isUndefined(Comp)) return <NoFoundPage />
