c6c339bca1c493eb4a4be2616f3729fe6e52be8a
韩旭gogogo <js_bin@163.com>
2022-11-25 14:04:19 +0800

    feature: abtest logic add

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import { DefaultFooter } from '@ant-design/pro-layout'
 
+// === Hooks === //
+import useABTest from '@/hooks/useABTest'
+
 // === Utils === //
 import { isMarketingHost } from '@/utils/location'
+
+// === Constants === //
+import { MAIL_HIDDEN } from '@/constants/abtest'
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   const isMarketing = isMarketingHost()
+  const { [MAIL_HIDDEN]: mailHidden } = useABTest()
   return (
     <DefaultFooter
       copyright={`Produced by Bank Of Chain ${currentYear}`}
-      links={[
-        {
-          key: 'BoC Homepage',
-          title: 'BoC Homepage',
-          href: isMarketing ? 'https://bankofchain.io/' : IMAGE_ROOT,
-          blankTarget: true
-        },
-        {
-          key: 'BoC Dashboard',
-          title: 'BoC Dashboard',
-          href: isMarketing ? 'https://dashboard.bankofchain.io/' : DASHBOARD_ROOT,
-          blankTarget: true
-        }
-      ]}
+      links={
+        !mailHidden && [
+          {
+            key: 'BoC Homepage',
+            title: 'BoC Homepage',
+            href: isMarketing ? 'https://bankofchain.io/' : IMAGE_ROOT,
+            blankTarget: true
+          },
+          {
+            key: 'BoC Dashboard',
+            title: 'BoC Dashboard',
+            href: isMarketing ? 'https://dashboard.bankofchain.io/' : DASHBOARD_ROOT,
+            blankTarget: true
+          }
+        ]
+      }
     />
   )
 }
