6757b1ebbc1d5da9a1353d1e7cd6b53cf5d65a03
韩旭gogogo <js_bin@163.com>
2022-12-19 19:51:41 +0800

    feature: add text utils for risk text

---
+++ src/utils/text-utils.js
@@ -0,0 +1,13 @@
+// template of warning text
+
+export const riskText1 = text => {
+  return `${text} versus USD unanchoring risk`
+}
+
+export const riskText2 = (coin1, coin2) => {
+  return `Risk of ${coin1} depeg with ${coin2}`
+}
+
+export const riskText3 = (coin = 'Eth') => {
+  return `${coin} liquidation risk`
+}
