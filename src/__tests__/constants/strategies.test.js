0f8d04e3c9bafca4007b8b086fd067253e13e608
韩旭gogogo <js_bin@163.com>
2023-01-06 11:35:34 +0800

    test: testcase update

--- src/__tests__/constants/strategies.test.js
+++ src/__tests__/constants/strategies.test.js
@@ -8,6 +8,6 @@ test('matic usdi has 12 platform', () => {
   expect(USDI_MATIC_STRATEGIES_MAP.length).toBe(12)
 })
 
-test('eth ethi has 10 platform', () => {
-  expect(ETHI_ETH_STRATEGIES_MAP.length).toBe(10)
+test('eth ethi has 11 platform', () => {
+  expect(ETHI_ETH_STRATEGIES_MAP.length).toBe(11)
 })
