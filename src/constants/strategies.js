17ef8e401e5e6cfeb0ac2568017f70051832face
韩旭gogogo <js_bin@163.com>
2023-03-01 18:45:15 +0800

    chore: add Frax icon

--- src/constants/strategies.js
+++ src/constants/strategies.js
@@ -34,7 +34,22 @@ const USDI_MATIC_STRATEGIES_MAP = [
   'UniswapV3'
 ]
 
-const ETHI_ETH_STRATEGIES_MAP = ['Balancer', 'UniswapV2', 'Convex', 'Aura', 'UniswapV3', 'StakeWise', 'YearnV2', 'Aave', 'DForce', 'Euler', 'Stargate']
+const ETHI_ETH_STRATEGIES_MAP = [
+  'Balancer',
+  'UniswapV2',
+  'Convex',
+  'Aura',
+  'UniswapV3',
+  'StakeWise',
+  'YearnV2',
+  'Aave',
+  'DForce',
+  'Euler',
+  'Stargate',
+  'Alpha',
+  'Gearbox',
+  'Frax'
+]
 
 export const USDI_STRATEGIES_MAP = {
   1: USDI_ETH_STRATEGIES_MAP,
