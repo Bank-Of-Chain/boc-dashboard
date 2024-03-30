f872ff3f36a24ab50a522040996966ebaf610bcd
蒋程鑫 <786255@nd.com>
2024-03-30 23:28:09 +0800

    robot

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { useModel } from 'umi'
 
 // === Components === //
@@ -7,6 +7,9 @@ import USDi from './usdi'
 
 // === Constants === //
 import { VAULT_TYPE } from '@/constants/vault'
+import s from './style.less'
+import { Popover, Button } from 'antd'
+import Typed from 'typed.js'
 
 export default function Home() {
   const { initialState } = useModel('@@initialState')
@@ -15,5 +18,50 @@ export default function Home() {
     [VAULT_TYPE.ETHi]: ETHi
   }[initialState.vault]
 
-  return <Comp />
+  const [open, setOpen] = useState(false)
+  const [showConfirm, setShowConfirm] = useState(false)
+  const ref = useRef(null)
+  const handleOpenChange = newOpen => {
+    console.log('newOpen', newOpen)
+    setOpen(newOpen)
+    if (newOpen) {
+      setTimeout(() => {
+        new Typed(ref.current, {
+          strings: ['I wanna deposit 1 APT to the APT vault.'],
+          typeSpeed: 50,
+          onComplete: () => {
+            setShowConfirm(true)
+          }
+        })
+      }, 3000)
+    }
+  }
+
+  const toDeposit = () => {
+    window.location.href = 'http://localhost:3000'
+  }
+
+  return (
+    <div>
+      <Comp />
+      <div className={s.robot}>
+        <Popover
+          placement="topRight"
+          content={
+            <div className={s.popover}>
+              <span className={s.loader} />
+              <span ref={ref} />
+              {showConfirm && <Button size="large" onClick={toDeposit}>Confirm</Button>}
+            </div>
+          }
+          title=""
+          trigger="click"
+          open={open}
+          onOpenChange={handleOpenChange}
+        >
+          <img src="/robot.png" alt="" className={s.icon} />
+        </Popover>
+      </div>
+    </div>
+  )
 }
